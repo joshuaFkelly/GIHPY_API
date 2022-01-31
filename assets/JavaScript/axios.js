2d8ad0fe15e87c7c5255b147ae3533944e405bf3
Joshua <jdkelly2015@gmail.com>
2022-01-30 19:24:18 -0500

    idk how i did it but i am able to click on a button and search the giphy API based on the buttons ID

--- assets/JavaScript/axios.js
+++ assets/JavaScript/axios.js
@@ -1,12 +1,36 @@
 const giphsSection = document.querySelector("#giphs");
-function getAnime() {
+const btnGroup = document.querySelector("#buttons");
+const topics = [
+  "Naruto",
+  "CowBoy Bebop",
+  "Dragon Ball Z",
+  "Dragon Bakk Super",
+  "Attack on Titan",
+  "Inuyasha",
+  "Bleach",
+  "Naruto Shippuden",
+  "Dragon Ball GT",
+  "Howls Moving Castle",
+  "Spirited Away",
+  "Seven Deadly Sins",
+  "A Quiet World",
+  "Death Note",
+  "Baki",
+  "Black Butler",
+  "Evangelion",
+  "Full Metal Alchemist",
+];
+
+// Get Request
+function getAnime(giphName) {
+  let giph = giphName.target.id;
   axios
     .get(
-      "https://api.giphy.com/v1/gifs/search?api_key=q8DYV0M8eXqbtdQxXfnOJHMbFjtuG0Gz&q=cowboybebop&limit=10&offset=0&rating=r&lang=en"
+      `https://api.giphy.com/v1/gifs/search?api_key=q8DYV0M8eXqbtdQxXfnOJHMbFjtuG0Gz&q=${giph}&limit=10&offset=0&rating=r&lang=en`
     )
-    .then(function (response) {
+    .then(function (res) {
       // handle success
-      showOutput(response);
+      showOutput(res);
     })
     .catch(function (error) {
       // handle error
@@ -16,16 +40,16 @@ function getAnime() {
       // always executed
     });
 }
-getAnime();
 
-function showOutput(response) {
-  const giphsArray = response.data.data;
+// Display Giphs
+function showOutput(res) {
+  const giphsArray = res.data.data;
   giphsArray.forEach((giph, index, giphs) => {
     giphsSection.innerHTML += `
                 <div class="col-6">
                       <div class="card">
                           <header class="card-header text-center">
-                              ${giphs[index].title}
+                             <h5> ${giphs[index].title} </h5>
                           </header>
                           <img src= "${giphs[index].images.original.url}" class="card-img-top bg-dark" alt="GIPHY">
                       </div>
@@ -33,3 +57,14 @@ function showOutput(response) {
       `;
   });
 }
+
+// Buttons
+topics.forEach((button, index, buttons) => {
+  const giphyBtn = document.createElement("button");
+  giphyBtn.setAttribute("type", "button");
+  giphyBtn.addEventListener("click", getAnime);
+  giphyBtn.classList = "btn btn-outline-primary m-1";
+  giphyBtn.id = `${buttons[index]}`;
+  giphyBtn.textContent += buttons[index];
+  btnGroup.append(giphyBtn);
+});
--- assets/JavaScript/giphCard.js
+++ assets/JavaScript/giphCard.js
@@ -1,10 +1,17 @@
 const btnGroup = document.querySelector("#giphs");
 
-topics.forEach((button, index, buttons) => {
-  const giphyBtn = document.createElement("button");
-  giphyBtn.setAttribute("type", "button");
-  //   giphyBtn.addEventListener("click");
-  giphyBtn.classList = "btn btn-outline-primary m-1";
-  giphyBtn.textContent += buttons[index];
-  btnGroup.append(giphyBtn);
-});
+function showOutput(res) {
+  const giphsArray = res.data.data;
+  giphsArray.forEach((giph, index, giphs) => {
+    giphsSection.innerHTML += `
+                <div class="col-6">
+                      <div class="card">
+                          <header class="card-header text-center">
+                              ${giphs[index].title}
+                          </header>
+                          <img src= "${giphs[index].images.original.url}" class="card-img-top bg-dark" alt="GIPHY">
+                      </div>
+                  </div>
+      `;
+  });
+}
